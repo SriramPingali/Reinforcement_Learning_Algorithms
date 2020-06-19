419ce8ec797fd26dcbcc75e11a20ed9dd5e4ccf9
Sriram Pingali <sriram26354@gmail.com>
2020-06-19 14:55:48 +0530

    Add updated dqn_agent.py

--- dqn_agent.py
+++ dqn_agent.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e5)  # replay buffer size
+BUFFER_SIZE = int(1e4)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
